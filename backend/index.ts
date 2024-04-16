194a2a4d86758476bdba9d6682da53529caf1072
Alikhan Kubatbekov <alixan.kubatbekov@gmail.com>
2024-04-16 15:35:04 +0600

    Initial commit for backend

---
+++ backend/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import config from './config';
+
+const app = express();
+const PORT = 8008;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+const run = async () => {
+  await mongoose.connect(config.mongoose.db);
+
+  app.listen(PORT, () => {
+    console.log(`Server started on ${PORT} port`);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+};
+
+void run();
