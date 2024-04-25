eaa3c3d2f9c823bb6e63bc249cc72d3848b35a85
Alikhan Kubatbekov <alixan.kubatbekov@gmail.com>
2024-04-25 21:14:52 +0600

    HW-85: Initial commit for frontend

---
+++ frontend/src/theme.ts
@@ -0,0 +1,15 @@
+import {createTheme} from '@mui/material';
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 800,
+      lg: 1100,
+      xl: 1536,
+    },
+  },
+});
+
+export default theme;
