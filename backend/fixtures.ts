29429e3855b9b86803ffb8afe0eb27b4e5b9e893
Alikhan Kubatbekov <alixan.kubatbekov@gmail.com>
2024-05-08 20:51:51 +0600

    #HW-89: Добавил фикстуры

--- backend/fixtures.ts
+++ backend/fixtures.ts
@@ -36,6 +36,7 @@ const abyssTracksNames = ['Abyss', 'Pulse', 'Pulse', 'Translucid', 'Dark Beach']
 const etherealityTracksNames = ['Possession', 'Emotion', '3NDL3SS', 'Sakura', 'Iris'];
 const theWorryTracksNames = ['Back Out', 'Another', 'Hands', 'Test & Recognise', 'Boys'];
 const domeTracksNames = ['Go', '3', 'Blood Bank', 'Reset Head', 'Mingus'];
+const gabrielTracksNames = ['ANGOSTURA', 'Limbo', 'Understand'];
 
 const run = async () => {
   await mongoose.connect(config.mongoose.db);
@@ -57,12 +58,17 @@ const run = async () => {
     role: 'user'
   });
 
-  const [pastelGhostArtist, seekaeArtist] = await Artist.create({
+  const [
+    pastelGhostArtist,
+    seekaeArtist,
+    keshiArtist
+  ] = await Artist.create({
     name: 'Pastel Ghost',
     photo: 'fixtures/pastelGhostPhoto.jpeg',
     information: `PASTEL GHOST is a synthpop artist from Oakland, California, and currently based in Austin, Texas. 
     She self-released two EPs, Dark Beach EP and Shadows EP, 
-    before signing to 80s Ladies Records for the release of her debut full-length album, Abyss.`
+    before signing to 80s Ladies Records for the release of her debut full-length album, Abyss.`,
+    isPublished: true,
   }, {
     name: 'Seekae',
     photo: 'fixtures/seekaePhoto.jpeg',
@@ -70,34 +76,52 @@ const run = async () => {
     They formed the group under the name Commander Keen in reference to the early-'90s MS-DOS video game series, 
     later changing the name to an elongated version of Commander Keen's initials (CK) after realising 
     the name was taken by a Scottish band. They released their debut album The Sound of Trees Falling 
-    on People in 2008 and released the follow-up +Dome in 2011 and the EP 3 in 2012.`
+    on People in 2008 and released the follow-up +Dome in 2011 and the EP 3 in 2012.`,
+    isPublished: true,
+  }, {
+    name: 'Keshi',
+    photo: 'fixtures/keshiPhoto.jpeg',
+    information: `Keshi is a Vietnamese-American artist under Island Records. 
+    In 2017, he had quit his nursing job to pursue music full time.`,
+    isPublished: false,
   });
 
   const [
     abyssAlbum,
     etherealityAlbum,
     theWorryAlbum,
-    domeAlbum
+    domeAlbum,
+    gabrielAlbum
   ] = await Album.create({
     title: 'Abyss',
     artist: pastelGhostArtist,
     publicDate: 2015,
-    albumImage: 'fixtures/abyssAlbumImage.jpeg'
+    albumImage: 'fixtures/abyssAlbumImage.jpeg',
+    isPublished: true,
   }, {
     title: 'Ethereality',
     artist: pastelGhostArtist,
     publicDate: 2018,
     albumImage: 'fixtures/etherealityAlbumImage.jpeg',
+    isPublished: true,
   }, {
     title: 'The worry',
     artist: seekaeArtist,
     publicDate: 2014,
-    albumImage: 'fixtures/theWorryAlbumImage.jpeg'
+    albumImage: 'fixtures/theWorryAlbumImage.jpeg',
+    isPublished: true,
   }, {
     title: '+Dome',
     artist: seekaeArtist,
     publicDate: 2011,
-    albumImage: 'fixtures/+domeAlbumImage.jpeg'
+    albumImage: 'fixtures/+domeAlbumImage.jpeg',
+    isPublished: true,
+  }, {
+    title: 'GABRIEL',
+    artist: keshiArtist,
+    publicDate: 2022,
+    albumImage: 'fixtures/gabrielAlbumImage.png',
+    isPublished: false,
   });
 
   for (let trackNumber = 0; trackNumber < abyssTracksNames.length; trackNumber++) {
@@ -105,7 +129,8 @@ const run = async () => {
       trackNumber: trackNumber + 1,
       trackName: abyssTracksNames[trackNumber],
       album: abyssAlbum,
-      trackDuration: getRandomTrackDuration()
+      trackDuration: getRandomTrackDuration(),
+      isPublished: true,
     });
   }
 
@@ -114,7 +139,8 @@ const run = async () => {
       trackNumber: trackNumber + 1,
       trackName: etherealityTracksNames[trackNumber],
       album: etherealityAlbum,
-      trackDuration: getRandomTrackDuration()
+      trackDuration: getRandomTrackDuration(),
+      isPublished: true,
     });
   }
 
@@ -123,7 +149,8 @@ const run = async () => {
       trackNumber: trackNumber + 1,
       trackName: theWorryTracksNames[trackNumber],
       album: theWorryAlbum,
-      trackDuration: getRandomTrackDuration()
+      trackDuration: getRandomTrackDuration(),
+      isPublished: true,
     });
   }
 
@@ -132,7 +159,18 @@ const run = async () => {
       trackNumber: trackNumber + 1,
       trackName: domeTracksNames[trackNumber],
       album: domeAlbum,
-      trackDuration: getRandomTrackDuration()
+      trackDuration: getRandomTrackDuration(),
+      isPublished: true,
+    });
+  }
+
+  for (let trackNumber = 0; trackNumber < gabrielTracksNames.length; trackNumber++) {
+    await Track.create({
+      trackNumber: trackNumber + 1,
+      trackName: gabrielTracksNames[trackNumber],
+      album: gabrielAlbum,
+      trackDuration: getRandomTrackDuration(),
+      isPublished: false,
     });
   }
 
